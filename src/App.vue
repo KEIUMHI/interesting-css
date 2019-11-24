4c3e82995e1de3ce2e44256ee25d918e8c93d906
keiumhi <1528158315@qq.com>
2019-11-24 23:05:51 +0800

    modify layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
-    <Guide msg="Welcome to Your Vue.js App"/>
+    <Guide msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import Guide from '@/components/guide'
-export default {
-  name: 'app',
-  components: {
-    Guide
+  import Guide from '@/components/guide'
+  export default {
+    name: 'app',
+    components: {
+      Guide
+    }
   }
-}
 </script>
 
-<style>
+<style lang="less">
+  #app {
+    width: 100vw;
+    height: 100vh;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-size: 16px;
+    color: #2c3e50;
+  }
 </style>
--- src/components/guide.vue
+++ src/components/guide.vue
@@ -1,20 +1,61 @@
 <template>
-   <div class="guide-ctn">
-     <router-link to="/bubbly">bubbly</router-link>
-     <router-link to="/title">title</router-link>
-     <router-link to="/checkbox">checkbox</router-link>
-     <router-link to="/taiji">taiji</router-link>
-     <router-link to="/radio">radio</router-link>
-     <router-link to="/color-change">color-change</router-link>
-     <router-view></router-view>
-   </div>
+  <ul class="guide-ctn">
+    <div class="demo-list">
+      <li v-for="(demo, index) in demos"
+          :key="index"
+          class="item"
+          @click="show(demo)">{{ demo }}</li>
+    </div>
+    <div class="demo-show">
+      <router-view></router-view>
+    </div>
+  </ul>
 </template>
 
 <script>
-export default {
-  name: 'guide'
-}
+  export default {
+    name: 'guide',
+    data() {
+      return {
+        demos: ['bubbly',
+          'title',
+          'checkbox',
+          'taiji',
+          'radio',
+          'color-change'
+        ]
+      }
+    },
+    methods: {
+      show(demo) {
+        this.$router.push(`/${demo}`)
+      }
+    }
+  }
 </script>
 
 <style lang="less" scoped>
+  .guide-ctn {
+    display: flex;
+    margin: 0;
+    height: 100%;
+  }
+
+
+  .demo-list {
+    flex: 2;
+    .item {
+      transition: color .1s;
+      cursor: pointer;
+
+      &:hover {
+        color: #3eaf7c;
+      }
+    }
+  }
+
+  .demo-show {
+    flex: 8;
+    position: relative;
+  }
 </style>
